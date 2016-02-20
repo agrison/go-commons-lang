be3593e5ff01901ce2117b8ae27c839fcd5394ba
Alexandre Grison <a.grison@gmail.com>
2016-02-20 19:00:27 +0100

    golint

--- mathUtils/fraction.go
+++ mathUtils/fraction.go
@@ -8,36 +8,66 @@ import (
 
 // Definitely needs testing!
 type Fraction struct {
-	// The numerator number part of the fraction (the three in three sevenths).
+	// The numerator number part of the fraction (the Three in Three sevenths).
 	numerator int
-	// The denominator number part of the fraction (the seven in three sevenths).
+	// The denominator number part of the fraction (the seven in Three sevenths).
 	denominator int
 }
 
-var ZERO = NewFraction(0, 1)
-var ONE = NewFraction(1, 1)
-var ONE_HALF = NewFraction(1, 2)
-var ONE_THIRD = NewFraction(1, 3)
-var TWO_THIRDS = NewFraction(2, 3)
-var ONE_QUARTER = NewFraction(1, 4)
-var TWO_QUARTERS = NewFraction(2, 4)
-var THREE_QUARTERS = NewFraction(3, 4)
-var ONE_FIFTH = NewFraction(1, 5)
-var TWO_FIFTHS = NewFraction(2, 5)
-var THREE_FIFTHS = NewFraction(3, 5)
-var FOUR_FIFTHS = NewFraction(4, 5)
+// Zero is 0/1
+var Zero = NewFraction(0, 1)
 
+// One is 1/1
+var One = NewFraction(1, 1)
+
+// OneHafl is 1/2
+var OneHalf = NewFraction(1, 2)
+
+// OneThird is 1/3
+var OneThird = NewFraction(1, 3)
+
+// TwoThirds is 2/3
+var TwoThirds = NewFraction(2, 3)
+
+// OneQuarter is 1/4
+var OneQuarter = NewFraction(1, 4)
+
+// TwoQuarters is 2/4
+var TwoQuarters = NewFraction(2, 4)
+
+// ThreeQuarters is 3/4
+var ThreeQuarters = NewFraction(3, 4)
+
+// OneFifth is 1/5
+var OneFifth = NewFraction(1, 5)
+
+// TwoFifths is 2/5
+var TwoFifths = NewFraction(2, 5)
+
+// ThreeFifths is 3/5
+var ThreeFifths = NewFraction(3, 5)
+
+// FourFifths is 4/5
+var FourFifths = NewFraction(4, 5)
+
+// MaxUint is the maximum uint value
 const MaxUint = ^uint(0)
+
+// MinUint is the minimum uint value (0)
 const MinUint = 0
+
+// MaxInt is the maximum int value
 const MaxInt = int(^uint(0) >> 1)
+
+// MinInt is the minimum int value
 const MinInt = -(MaxInt - 1)
 
-// Constructs a Fraction instance with the 2 parts of a fraction Y/Z.
+// NewFraction constructs a Fraction instance with the 2 parts of a fraction Y/Z.
 func NewFraction(numerator, denominator int) *Fraction {
 	return &Fraction{numerator, denominator}
 }
 
-// Creates a Fraction instance with the 2 parts of a fraction Y/Z.
+// GetFraction creates a Fraction instance with the 2 parts of a fraction Y/Z.
 func GetFraction(numerator, denominator int) *Fraction {
 	if denominator == 0 {
 		return nil
@@ -52,7 +82,7 @@ func GetFraction(numerator, denominator int) *Fraction {
 	return NewFraction(numerator, denominator)
 }
 
-// Creates a Fraction instance with the 3 parts of a fraction X Y/Z.
+// GetWholeFraction creates a Fraction instance with the 3 parts of a fraction X Y/Z.
 func GetWholeFraction(whole, numerator, denominator int) *Fraction {
 	if denominator == 0 {
 		return nil
@@ -75,14 +105,14 @@ func GetWholeFraction(whole, numerator, denominator int) *Fraction {
 	return NewFraction(int(numeratorValue), denominator)
 }
 
-// Creates a reduced Fraction instance with the 2 parts of a fraction Y/Z.
+// GetReducedFraction creates a reduced Fraction instance with the 2 parts of a fraction Y/Z.
 // For example, if the input parameters represent 2/4, then the created fraction will be 1/2.
 func GetReducedFraction(numerator, denominator int) *Fraction {
 	if denominator == 0 {
 		return nil
 	}
 	if numerator == 0 {
-		return ZERO
+		return Zero
 	}
 	if denominator == MinInt && (numerator&1) == 0 {
 		numerator = numerator / 2
@@ -102,19 +132,19 @@ func GetReducedFraction(numerator, denominator int) *Fraction {
 	return NewFraction(numerator, denominator)
 }
 
-// Checks if two Fraction are the same
+// Equals checks if Two Fraction are the same
 func (f *Fraction) Equals(f2 *Fraction) bool {
 	return f.numerator == f2.numerator && f.denominator == f2.denominator
 }
 
-// Reduce the fraction to the smallest values for the numerator and denominator, returning the result.
+// Reduce reduce the fraction to the smallest values for the numerator and denominator, returning the result.
 // For example, if this fraction represents 2/4, then the result will be 1/2.
 func (f *Fraction) Reduce() *Fraction {
 	if f.numerator == 0 {
-		if f.Equals(ZERO) {
+		if f.Equals(Zero) {
 			return f
 		} else {
-			return ZERO
+			return Zero
 		}
 	}
 	gcd := greatestCommonDivisor(int(math.Abs(float64(f.numerator))), f.denominator)
@@ -124,11 +154,11 @@ func (f *Fraction) Reduce() *Fraction {
 	return GetFraction(f.numerator/gcd, f.denominator/gcd)
 }
 
-// Gets a fraction that is the inverse (1/fraction) of this one.
+// Invert gets a fraction that is the inverse (1/fraction) of this One.
 // The returned fraction is not reduced.
 func (f *Fraction) Invert() *Fraction {
 	if f.numerator == 0 {
-		panic("unable to invert zero")
+		panic("unable to invert Zero")
 	}
 	if f.numerator == MinInt {
 		panic("can't negate numerator")
@@ -139,7 +169,7 @@ func (f *Fraction) Invert() *Fraction {
 	return NewFraction(f.denominator, f.numerator)
 }
 
-// Gets a fraction that is the negative (-fraction) of this one.
+// Negate gets a fraction that is the negative (-fraction) of this One.
 // The returned fraction is not reduced.
 func (f *Fraction) Negate() *Fraction {
 	if f.numerator == MinInt {
@@ -148,7 +178,7 @@ func (f *Fraction) Negate() *Fraction {
 	return NewFraction(-f.numerator, f.denominator)
 }
 
-// Gets a fraction that is the positive equivalent of this one.
+// Abs gets a fraction that is the positive equivalent of this One.
 // More precisely: fraction >= 0 ? this : -fraction)
 // The returned fraction is not reduced.
 func (f *Fraction) Abs() *Fraction {
@@ -158,11 +188,12 @@ func (f *Fraction) Abs() *Fraction {
 	return f.Negate()
 }
 
+// Pow get a fraction that is powered by a specific value
 func (f *Fraction) Pow(power int) *Fraction {
 	if power == 1 {
 		return f
 	} else if power == 0 {
-		return ONE
+		return One
 	} else if power < 0 {
 		if power == MinInt {
 			return f.Invert().Pow(2).Pow(-(power / 2))
@@ -178,10 +209,10 @@ func (f *Fraction) Pow(power int) *Fraction {
 	}
 }
 
-// Multiplies the value of this fraction by another, returning the result in reduced form.
+// MultiplyBy multiplies the value of this fraction by another, returning the result in reduced form.
 func (f *Fraction) MultiplyBy(ff *Fraction) *Fraction {
 	if f.numerator == 0 || ff.numerator == 0 {
-		return ZERO
+		return Zero
 	}
 	// knuth 4.5.1
 	// make sure we don't overflow unless the result *must* overflow.
@@ -193,55 +224,55 @@ func (f *Fraction) MultiplyBy(ff *Fraction) *Fraction {
 	return GetReducedFraction(a, b)
 }
 
-// Divide the value of this fraction by another.
+// DivideBy divides the value of this fraction by another.
 func (f *Fraction) DivideBy(ff *Fraction) *Fraction {
 	if ff.numerator == 0 {
-		panic("The fraction to divide by must not be zero")
+		panic("The fraction to divide by must not be Zero")
 	}
 	return f.MultiplyBy(ff.Invert())
 }
 
-// Gets the numerator part of the fraction.
+// GetNumerator gets the numerator part of the fraction.
 // This method may return a value greater than the denominator, an improper fraction, such as the seven in 7/4.
 func (f *Fraction) GetNumerator() int {
 	return f.numerator
 }
 
-// Gets the denominator part of the fraction.
+// GetDenominator gets the denominator part of the fraction.
 func (f *Fraction) GetDenominator() int {
 	return f.denominator
 }
 
-// Gets the proper numerator, always positive.
-// An improper fraction 7/4 can be resolved into a proper one, 1 3/4. This method returns the 3 from the proper fraction.
+// GetProperNumerator gets the proper numerator, always positive.
+// An improper fraction 7/4 can be resolved into a proper One, 1 3/4. This method returns the 3 from the proper fraction.
 // If the fraction is negative such as -7/4, it can be resolved into -1 3/4, so this method returns the positive proper numerator, 3.
 func (f *Fraction) GetProperNumerator() int {
 	return int(math.Abs(float64(f.numerator % f.denominator)))
 }
 
-// Gets the proper whole part of the fraction.
-// An improper fraction 7/4 can be resolved into a proper one, 1 3/4. This method returns the 1 from the proper fraction.
+// GetProperWhole gets the proper whole part of the fraction.
+// An improper fraction 7/4 can be resolved into a proper One, 1 3/4. This method returns the 1 from the proper fraction.
 // If the fraction is negative such as -7/4, it can be resolved into -1 3/4, so this method returns the positive whole part -1.
 func (f *Fraction) GetProperWhole() int {
 	return f.numerator / f.denominator
 }
 
-// Gets the fraction as an int. This returns the whole number part of the fraction.
+// IntValue gets the fraction as an int. This returns the whole number part of the fraction.
 func (f *Fraction) IntValue() int {
 	return f.numerator / f.denominator
 }
 
-// Gets the fraction as a float32. This calculates the fraction as the numerator divided by denominator.
+// Float32Value gets the fraction as a float32. This calculates the fraction as the numerator divided by denominator.
 func (f *Fraction) Float32Value() float32 {
 	return float32(f.numerator) / float32(f.denominator)
 }
 
-// Gets the fraction as a float64. This calculates the fraction as the numerator divided by denominator.
+// Float64Value gets the fraction as a float64. This calculates the fraction as the numerator divided by denominator.
 func (f *Fraction) Float64Value() float64 {
 	return float64(f.numerator) / float64(f.denominator)
 }
 
-// Gets the greatest common divisor of the absolute value of two numbers, using the "binary gcd" method which avoids
+// greatestCommonDivisor gets the greatest common divisor of the absolute value of Two numbers, using the "binary gcd" method which avoids
 // division and modulo operations. See Knuth 4.5.2 algorithm B. This algorithm is due to Josef Stein (1961).
 func greatestCommonDivisor(u, v int) int {
 	// From Commons Math:
@@ -268,7 +299,7 @@ func greatestCommonDivisor(u, v int) int {
 	// B1. [Find power of 2]
 	k := 0
 	for (u&1) == 0 && (v&1) == 0 && k < 31 { // while u and v are both even...
-		u = u / 2 // cast out twos.
+		u = u / 2 // cast out Twos.
 		v = v / 2
 		k += 1
 	}
@@ -276,7 +307,7 @@ func greatestCommonDivisor(u, v int) int {
 		panic("gcd is 2^31")
 	}
 	// B2. Initialize: u and v have been divided by 2^k and at least
-	//     one is odd.
+	//     One is odd.
 	t := 0
 	if (u & 1) == 1 {
 		t = v
@@ -288,9 +319,9 @@ func greatestCommonDivisor(u, v int) int {
 	foo := true
 	for foo || (t != 0) {
 		foo = false
-		// B4/B3: cast out twos from t.
+		// B4/B3: cast out Twos from t.
 		for (t & 1) == 0 { // while t is even..
-			t = t / 2 // cast out twos
+			t = t / 2 // cast out Twos
 		}
 		// B5 [reset max(u,v)]
 		if t > 0 {
@@ -306,7 +337,7 @@ func greatestCommonDivisor(u, v int) int {
 	return -u * (1 << uint(k)) // gcd is u*2^k
 }
 
-// Multiply two integers, checking for overflow.
+// mulAndCheck multiply two integers, checking for overflow.
 func mulAndCheck(x int, y int) int {
 	m := int64(x) * int64(y)
 	if m < int64(MinInt) || m > int64(MaxInt) {
@@ -315,7 +346,7 @@ func mulAndCheck(x int, y int) int {
 	return int(m)
 }
 
-// Multiply two non-negative integers, checking for overflow.
+// mulPosAndCheck multiply two non-negative integers, checking for overflow.
 func mulPosAndCheck(x int, y int) int {
 	m := int64(x) * int64(y)
 	if m > int64(MaxInt) {
@@ -324,7 +355,7 @@ func mulPosAndCheck(x int, y int) int {
 	return int(m)
 }
 
-// Add two integers, checking for overflow.
+// addAndCheck add two integers, checking for overflow.
 func addAndCheck(x int, y int) int {
 	s := int64(x) + int64(y)
 	if s < int64(MinInt) || s > int64(MaxInt) {
@@ -333,7 +364,7 @@ func addAndCheck(x int, y int) int {
 	return int(s)
 }
 
-// Subtract two integers, checking for overflow.
+// subAndCheck subtract two integers, checking for overflow.
 func subAndCheck(x int, y int) int {
 	s := int64(x) - int64(y)
 	if s < int64(MinInt) || s > int64(MaxInt) {
@@ -342,9 +373,9 @@ func subAndCheck(x int, y int) int {
 	return int(s)
 }
 
-// Implement add and subtract using algorithm described in Knuth 4.5.1.
+// addSub implement add and subtract using algorithm described in Knuth 4.5.1.
 func addSub(f *Fraction, ff *Fraction, isAdd bool) *Fraction {
-	// zero is identity for addition.
+	// Zero is identity for addition.
 	if f.numerator == 0 {
 		if isAdd {
 			return ff
--- randUtils/randUtils.go
+++ randUtils/randUtils.go
@@ -11,30 +11,37 @@ func Seed() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
+// NextFloat64 returns the next float64.
 func NextFloat64() float64 {
 	return rand.Float64()
 }
 
+// NextFloat64Bounded returns the next float64 bounded by start and end.
 func NextFloat64Bounded(start float64, end float64) float64 {
 	return rand.Float64()*(end-start) + start
 }
 
+// NextFloat32 returns the next float32.
 func NextFloat32() float32 {
 	return rand.Float32()
 }
 
+// NextFloat32Bounded returns the next float32 bounded by start and end.
 func NextFloat32Bounded(start float32, end float32) float32 {
 	return rand.Float32()*(end-start) + start
 }
 
+// NextInt returns the next int.
 func NextInt() int {
 	return rand.Int()
 }
 
+// NextIntBounded returns the next int bounded by start and end.
 func NextIntBounded(start int, end int) int {
 	return start + rand.Intn(end)
 }
 
+// NextIntBounded returns the next int bounded by a maximum.
 func NextIntUpperBounded(end int) int {
 	return rand.Intn(end)
 }
@@ -42,7 +49,7 @@ func NextIntUpperBounded(end int) int {
 // NextBytes creates an array of random bytes.
 func NextBytes(count int) []byte {
 	a := make([]byte, count)
-	for i, _ := range a {
+	for i := range a {
 		a[i] = (byte)(NextInt())
 	}
 	return a
